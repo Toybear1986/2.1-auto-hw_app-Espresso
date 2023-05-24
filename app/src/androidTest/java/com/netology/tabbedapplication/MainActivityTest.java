82c68e6468b1b2ebc5dae844088b5e8d4b4e9333
Василий Скоробогатов <vaskorobogatov@yandex.ru>
2023-05-24 12:41:48 +0300

    Fix test Added checking opening first Tab after start app. Code cleanup - remove unused imports and method

--- app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
@@ -11,18 +11,11 @@ import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,35 +30,23 @@ public class MainActivityTest {
 
     @Test
     public void mainActivityTest() {
-        ViewInteraction tabView = onView(
-                allOf(withContentDescription("Tab 2"),
-                        isDisplayed()));
-        tabView.perform(click());
-
-        ViewInteraction textView = onView(
+        ViewInteraction page1 = onView(
                 allOf(withId(R.id.section_label),
                         withParent(allOf(withId(R.id.constraintLayout),
                                 withParent(withId(R.id.view_pager)))),
                         isDisplayed()));
-        textView.check(matches(withText("Page: 2")));
-    }
+        page1.check(matches(withText("Page: 1")));
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
+        ViewInteraction tab2 = onView(
+                allOf(withContentDescription("Tab 2"),
+                        isDisplayed()));
+        tab2.perform(click());
 
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        ViewInteraction page2 = onView(
+                allOf(withId(R.id.section_label),
+                        withParent(allOf(withId(R.id.constraintLayout),
+                                withParent(withId(R.id.view_pager)))),
+                        isDisplayed()));
+        page2.check(matches(withText("Page: 2")));
     }
 }
